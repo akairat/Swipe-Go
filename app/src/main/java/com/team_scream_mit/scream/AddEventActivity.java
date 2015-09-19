5ebd6effeccb5713181a717ca00b5c54a52f06b1
iveel <iveel6@yahoo.com>
2015-09-19 11:45:18 -0700

    forgot to import in addEvent

--- app/src/main/java/com/team_scream_mit/scream/AddEventActivity.java
+++ app/src/main/java/com/team_scream_mit/scream/AddEventActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
 
