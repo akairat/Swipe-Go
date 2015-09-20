4aa8ffda0e276aab0f21097fc6fef0a81e8a8f02
akairat <kairat.ashim@gmail.com>
2015-09-20 07:31:03 -0400

    app crashing fixed

--- app/src/main/java/com/team_scream_mit/scream/MainActivity.java
+++ app/src/main/java/com/team_scream_mit/scream/MainActivity.java
@@ -90,7 +90,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.drawable.icon_label);
 
         showLoadingDialog();
 
